54f5e0ee8732300c4a8782e572802b597edb8c7b
mbreshock <mbresho1@jh.edu>
2020-12-22 17:20:57 -0800

    adding initial Rproj files and first function

---
+++ R/load_co2_repo.R
@@ -0,0 +1,23 @@
+#' Load CO2 Emissions Repository
+#'
+#' Download the CO2 Emissions case study repository.
+#'
+#' @details This function downloads the OCS CO2 emissions case study repository
+#' on GitHub and saves it as a new R project in a local directory. This makes
+#' it so all of the raw and wrangled data is downloaded using one function.
+#' Need to have a personal access token (PAT) to work.
+#'
+#' @param outpath character string, path to the directory where the downloaded
+#' repository should be saved to.
+#'
+#' @return nothing useful is returned, a new R project will be loaded in the
+#' directory specified in outpath.
+#'
+#' @import usethis
+#' @export
+#'
+#' @examples load_co2_repo('/Users/michael/Desktop/')
+#'
+load_co2_repo <- function(outpath){
+  create_from_github('opencasestudies/ocs-bp-co2-emissions',destdir=outpath)
+}
