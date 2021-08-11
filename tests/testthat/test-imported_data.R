4070834f74a1c4aa19fa24e05bd7aa254b29e928
mbreshock <mbresho1@jh.edu>
2021-08-11 15:47:15 -0700

    fixed tests

--- tests/testthat/test-imported_data.R
+++ tests/testthat/test-imported_data.R
@@ -2,6 +2,6 @@ test_that("imported_data works", {
   tmp = tempfile()
   dir.create(tmp)
   imported_data('ocs-bp-opioid-rural-urban', outpath = tmp)
-  fpath = file.path(tmp,"OCS_data","data","import")
-  expect_equal(dir.exists(fpath), TRUE)
+  fpath = file.path(tmp,"OCS_data","data","imported","county_annual.rda")
+  expect_equal(file.exists(fpath), TRUE)
 })
--- tests/testthat/test-raw_data.R
+++ tests/testthat/test-raw_data.R
@@ -2,6 +2,6 @@ test_that("raw_data works", {
   tmp = tempfile()
   dir.create(tmp)
   raw_data('ocs-bp-opioid-rural-urban', outpath = tmp)
-  fpath = file.path(tmp,"OCS_data","data","raw")
-  expect_equal(dir.exists(fpath), TRUE)
+  fpath = file.path(tmp,"OCS_data","data","raw","LND01.xls")
+  expect_equal(file.exists(fpath), TRUE)
 })
